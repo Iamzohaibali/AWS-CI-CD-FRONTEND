26d1e60dc9d34d9222975ea936c898b4f4eff735
Iamzohaibali <j36740062@gmail.com>
2026-05-17 14:11:08 +0500

    env file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,31 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchApi = async () => {
       try {
         const response = await fetch(
-          "https://zohaib-ahmad-ali.tech/api"
+          `${import.meta.env.VITE_API_URL}/api`
         );
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch");
-        }
 
         const result = await response.json();
 
         setData(result);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
+      } catch (error) {
+        console.log(error);
       }
     };
 
     fetchApi();
   }, []);
 
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (error) {
-    return <h1>Error: {error}</h1>;
-  }
-
   return (
-    <div
-      style={{
-        padding: "40px",
-        fontFamily: "Arial",
-      }}
-    >
-      <h1>Frontend Connected To Backend 🚀</h1>
+    <div style={{ padding: "40px" }}>
+      <h1>Frontend + Backend Connected 🚀</h1>
 
-      <h2>{data.message}</h2>
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {data && <h2>{data.message}</h2>}
     </div>
   );
 };
